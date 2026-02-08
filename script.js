484fa69a397fc64a257c95b0f4c089b71a9fbcce
5ham <surajrxl06@gmail.com>
2026-02-08 23:24:11 +0530

    committed the initial layout and design

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // #FFC107
+
+let random_meal = fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+random_meal
+    .then((response)=> {
+        return response.json()
+    })
+    .then((data)=> {
+        console.log(data);
+    })
+    .catch(function(error) {
+        console.log("Error occured, ",error)
+    })
