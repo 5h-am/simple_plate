fc619b2a329ccdb8e96150de2e0144f967d1091e
5ham <surajrxl06@gmail.com>
2026-02-09 21:52:13 +0530

    just before media query

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let animation_box = document.querySelector(".animation-box");
 let animation_nav = document.querySelector(".animation-nav");
 let filter = document.querySelector(".filter");
 let filter_options = document.querySelector(".filter-options")
-let  nav_options = document.querySelector(".nav-options");
+let nav_options = document.querySelector(".nav-options");
+let search_icon = document.querySelector(".search-icon");
+let search_box = document.querySelector(".search-box");
+let search_btn = document.querySelector(".search-btn");
+let back_icon = document.querySelector(".back-icon");
 
 function hidden (element) {
     element.classList.add("hidden");
@@ -71,23 +75,27 @@ promise
 
 filter.onclick = function() {
     visible(filter_options);
-    setTimeout(hidden, 10000,filter_options);
+    setTimeout(hidden, 5000,filter_options);
 }
 
 filtered_box.addEventListener("click", (e)=> {
     if (e.target.tagName = "BUTTON") {
-        hidden(nav_options)
-        hidden(animation_box)
-        hidden(animation_nav)
-        hidden(filtered_box)
+        setTimeout(hidden,200,nav_options)
+        setTimeout(hidden,200,animation_box)
+        setTimeout(hidden,200,animation_nav)
+        setTimeout(hidden,200,filtered_box) 
         let promise = fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${e.target.dataset.cat}`);
         promise
         .then((response) => {
             return response.json()
         }).then((data) => {
+            let span = document.createElement("span");
+            span.classList.add("material-symbols-outlined","back-icon");
+            span.textContent = "arrow_back";
             let h2 = document.createElement("h2");
             h2.classList.add("recipes-category");
             h2.textContent = e.target.dataset.cat;
+            recipes.appendChild(span);
             recipes.appendChild(h2);
             let meals = data["meals"];
             for (let i = 0; i<meals.length;i++) {
@@ -100,3 +108,27 @@ filtered_box.addEventListener("click", (e)=> {
         })
     }
 })
+
+search_icon.addEventListener("mouseover", ()=> {
+    setTimeout(visible,500,search_box)
+    setTimeout(visible,500,search_btn)
+    search_icon.style.visibility = "hidden";  
+})
+
+search_box.addEventListener("mouseout", () => {
+    setTimeout(hidden,500,search_box)
+    setTimeout(hidden,500,search_btn)
+    setTimeout(()=>{search_icon.style.visibility = "visible";},700)
+      
+})
+
+recipes.addEventListener("click", (e) => {
+    if(e.target.tagName = "SPAN") {
+        recipes.innerHTML = "";
+        setTimeout(visible,200,nav_options)
+        setTimeout(visible,200,animation_box)
+        setTimeout(visible,200,animation_nav)
+        setTimeout(visible,200,filtered_box) 
+    }
+})
+
